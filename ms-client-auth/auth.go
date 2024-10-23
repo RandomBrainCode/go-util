c1273fd73f7f92cb8db3d20c8dc7f31500b5faf9
Andrew Velasco <andrew.velasco@albertsons.com>
2024-10-22 23:47:55 -0600

    Add initial authentication module for MS Graph client
    This commit introduces `ms-client-auth`, an initial authentication module for interacting with Microsoft Graph API. It includes methods for obtaining credentials, generating auth URLs, and creating Graph clients. Additionally, it adds dependencies required for Azure and Microsoft Graph SDKs in go.mod.

---
+++ ms-client-auth/auth.go
@@ -0,0 +1,57 @@
+package ms_client_auth
+
+import (
+	"context"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/AzureAD/microsoft-authentication-library-for-go/apps/confidential"
+	msgraph "github.com/microsoftgraph/msgraph-sdk-go"
+	"github.com/microsoftgraph/msgraph-sdk-go-core/authentication"
+	"time"
+)
+
+type AuthParams struct {
+	clientID     string
+	clientSecret string
+	tenantID     string
+	authorityURL string
+	redirectURI  string
+	scopes       []string
+}
+
+type TokenCredential struct {
+	accessToken string
+}
+
+func (c *TokenCredential) GetToken(ctx context.Context, options policy.TokenRequestOptions) (azcore.AccessToken, error) {
+	return azcore.AccessToken{Token: c.accessToken, ExpiresOn: time.Now().Add(1 * time.Hour)}, nil
+}
+
+func GetCredential(clientSecret string) (confidential.Credential, error) {
+	return confidential.NewCredFromSecret(clientSecret)
+}
+
+func GetClient(clientID string, tenantID string, authorityURL string, credential confidential.Credential) (confidential.Client, error) {
+	if len(authorityURL) > 0 && authorityURL[len(authorityURL)-1] != '/' {
+		authorityURL = authorityURL + "/"
+	}
+	authority := authorityURL + tenantID
+
+	return confidential.New(authority, clientID, credential)
+}
+
+func GetAuthURL(appClient confidential.Client, clientID string, redirectURI string, scopes []string) (string, error) {
+	return appClient.AuthCodeURL(context.TODO(), clientID, redirectURI, scopes)
+}
+
+func GetAuthProvider(userToken *TokenCredential) (*authentication.AzureIdentityAuthenticationProvider, error) {
+	return authentication.NewAzureIdentityAuthenticationProvider(userToken)
+}
+
+func GetGraphAdapter(provider *authentication.AzureIdentityAuthenticationProvider) (*msgraph.GraphRequestAdapter, error) {
+	return msgraph.NewGraphRequestAdapter(provider)
+}
+
+func GetGraphClient(adapter *msgraph.GraphRequestAdapter) *msgraph.GraphServiceClient {
+	return msgraph.NewGraphServiceClient(adapter)
+}
